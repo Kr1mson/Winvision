8dac97362d8f807aa331cb4230c7d82893df9273
Parth Passi <parthpassi@gmail.com>
2024-07-06 15:07:25 +0530

    added input fields

--- script.js
+++ script.js
@@ -91,3 +91,6 @@ const observer = new IntersectionObserver((entries)=>{
 });
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el)=> observer.observe(el));
+
+
+
