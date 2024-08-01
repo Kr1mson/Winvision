757f42d92530c906af7e57f373f0cc0b1d70ef94
NotShivain <shivain65@gmail.com>
2024-08-02 00:13:30 +0530

    video

--- landing.js
+++ landing.js
@@ -7,7 +7,7 @@ const end = section.querySelector('h1');
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-  duration: 11000,
+  duration: 5000,
   triggerElement: intro,
   triggerHook: 0  
 }).addIndicators().setPin(intro).addTo(controller);
@@ -24,3 +24,22 @@ setInterval(() => {
   delay += (scrollpos - delay) * accelamount;
   video.currentTime = delay;
 }, 33.3);
+
+const itemHints = document.querySelector('.item-hints');
+const itemHints2 = document.querySelector('.item-hints2');
+
+// Initially hide the hints
+itemHints.style.display = 'none';
+itemHints2.style.display = 'none';
+
+// Listen for the end event of the scene
+scene.on('end', () => {
+  itemHints.style.display = 'block';
+  itemHints2.style.display = 'block';
+});
+
+// Listen for the leave event of the scene
+scene.on('leave', () => {
+  itemHints.style.display = 'none';
+  itemHints2.style.display = 'none';
+});
