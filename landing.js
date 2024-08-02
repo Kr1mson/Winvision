ae8297d5187a4d19df7b75c29f547d4cef757d28
NotShivain <shivain65@gmail.com>
2024-08-02 13:15:34 +0530

    more changes

--- landing.js
+++ landing.js
@@ -7,7 +7,7 @@ const end = section.querySelector('h1');
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-  duration: 600,
+  duration: 2000,
   triggerElement: intro,
   triggerHook: 0
 }).setPin(intro).addTo(controller);
